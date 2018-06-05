5504c613af81d1e0194bd6659908e4fe342a0bc7
Mark Fitzpatrick <markfitz815@gmail.com>
2018-06-05 16:59:37 +0100

    added function that creates list of elements necessary for list

--- alpha/ddScript.js
+++ alpha/ddScript.js
@@ -9,16 +9,30 @@ let newElDD = function(elData){
     if(elData.srce){newEl.src = elData.srce};
     if(elData.bgImage){newEl.style.backgroundImage="url('imgs/" + elData.bgImage + "')"};
     document.querySelector(elData.append).appendChild(newEl);
-    
 };
 
-let listItem = [
-    {type: 'div', class: ['listItemCon', 'listItemConLttP'], append: '.contain'},
-    {type: 'div', class: ['listItemImgCon', 'listItemImgConLttP'], append: '.listItemConLttP'},
-    {type: 'div', class: ['listItemImg', 'listItemImgLttP'], append: '.listItemImgConLttP', bgImage: 'ba-lttp.jpg'},
-    {type: 'div', class: ['listItemTextCon', 'listItemTextConLttP'], append: '.listItemConLttP'},
-    {type: 'div', class: ['listItemTitle', 'listItemTitleLttP'], append: '.listItemTextCon', inHL: 'Link to the Past'}
+
+let creListItem = function(itemData){
+    let newLi = [
+        {type: 'div', class: ['listItemCon', 'listItemCon'+itemData.name], append: '.contain'},
+        {type: 'div', class: ['listItemImgCon', 'listItemImgCon' + itemData.name], append: '.listItemCon' + itemData.name},
+        {type: 'div', class: ['listItemImg', 'listItemImg' + itemData.name], append: '.listItemImgCon' + itemData.name, bgImage: itemData.img},
+        {type: 'div', class: ['listItemTextCon', 'listItemTextCon' + itemData.name], append: '.listItemCon' + itemData.name},
+        {type: 'div', class: ['listItemTitle', 'listItemTitle' + itemData.name], append: '.listItemTextCon' + itemData.name, inHL: itemData.title},
+        {type: 'div', class: ['listItemDesc', 'listItemDesc' + itemData.name], append: '.listItemTextCon' + itemData.name},
+        {type: 'div', class: ['listItemConsole', 'listItemConsole' + itemData.name], append: '.listItemDesc' + itemData.name, inHL: '<b>Console: </b>' + itemData.cons},
+        {type: 'div', class: ['listItemYear', 'listItemYear' + itemData.name], append: '.listItemDesc' + itemData.name, inHL: '<b>Year: </b>' + itemData.year}
+    ];
+    listItemElementData.push(newLi);
+}
+
+let creListItemData = [
+    {name: 'LttP', img: 'ba-lttp.jpg', title: 'Link to the Past', cons: 'SNES', year: '1991'},
+    {name: 'OoT', img: 'ba-oot.jpg', title: 'Ocarina of Time', cons: 'Nintendo 64', year: '1998'},
+    {name: 'MM', img: 'ba-mm.jpg', title: "Majora's Mask", cons: 'Nintendo 64', year: '2000'}
 ]
+
+let listItemElementData = [];
 
 let elArr = [
     {type: 'div', class: ['contain'], append: '.ddApp'}
@@ -29,8 +43,15 @@ let elArr = [
 
 (function initApp(){
     newElDD(elArr[0]);
-    listItem.map(function(item){
-        newElDD(item);
+    creListItemData.map(function(item){
+        creListItem(item);
+    })
+    listItemElementData.map(function(item){
+        item.map(function(item2){
+            newElDD(item2);
+        })
+        
+        console.log(item)
     })
 })()
 
